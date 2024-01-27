b4b18daa603dc1a617d6b2ccc0862760a1c1a7b6
ia <isaac.ardis@gmail.com>
2024-01-27 07:07:30 -0700

    catTrackslib: refactor cors middleware for a subroute
    Date: 2024-01-27 07:07:30-07:00
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- router.go
+++ router.go
@@ -1,27 +1,42 @@
 package catTrackslib
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
+	router.Use(loggingMiddleware)
 
-	middleWarePermissiveCORS := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+	apiRoutes := router.NewRoute().Subrouter()
+	apiRoutes.Use(corsMiddleware)
 
-	})
-
-	router.Methods(http.MethodPost).Path("/populate/").HandlerFunc(populatePoints).
-		HandlerFunc(middleWarePermissiveCORS)
-	router.Methods(http.MethodGet).Path("/lastknown").HandlerFunc(getLastKnown).
-		HandlerFunc(middleWarePermissiveCORS)
-	router.Methods(http.MethodGet).Path("/catsnaps").HandlerFunc(handleGetCatSnaps).
-		HandlerFunc(middleWarePermissiveCORS)
+	apiRoutes.Methods(http.MethodPost).Path("/populate/").HandlerFunc(populatePoints)
+	apiRoutes.Methods(http.MethodGet).Path("/lastknown").HandlerFunc(getLastKnown)
+	apiRoutes.Methods(http.MethodGet).Path("/catsnaps").HandlerFunc(handleGetCatSnaps)
 
 	// File server merveres
 	ass := http.StripPrefix("/ass/", http.FileServer(http.Dir("./ass/")))
